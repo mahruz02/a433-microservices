1af5db138a634fad888f79d65a6576df2a494610
Ahmad Mahrus <learn.ahmadmahruz01.com>
2023-10-23 12:28:51 +0700

    fix bug

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,6 +4,6 @@
 docker build -t mahruz01/karsajobs-ui:latest -f Dockerfile .
 
 # Login to Docker Hub
-docker login -u mahruz01 -p
+docker login -u mahruz01
 # Push image to Docker Hub
 docker push mahruz01/karsajobs-ui:latest
