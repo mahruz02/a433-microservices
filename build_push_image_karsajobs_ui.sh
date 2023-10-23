ed3430e650087db6f02bd0dfe196f1752ddc6d08
Ahmad Mahrus <learn.ahmadmahruz01.com>
2023-10-23 11:49:22 +0700

    add kubernetes services

--- build_push_image_karsajobs_ui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build Docker image
-docker build -t mahruz01/karsajobs-ui:latest -f Dockerfile .
-
-# Login to Docker Hub
-docker login -u mahruz01
-# Push image to Docker Hub
-docker push mahruz01/karsajobs-ui:latest
