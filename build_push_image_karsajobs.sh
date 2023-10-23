30307c1ceb10a2c0212033e9537d99940c8bd77f
Ahmad Mahrus <learn.ahmadmahruz01.com>
2023-10-23 11:32:27 +0700

    add push image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t mahruz01/karsajobs:latest -f Dockerfile .
+
+# Login to Docker Hub
+docker login -u mahruz01
+
+# Push image to Docker Hub
+docker push mahruz01/karsajobs:latest
