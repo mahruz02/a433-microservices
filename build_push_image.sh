ab527fab17a0518fde0225890fac4245c2bf8aca
Ahmad Mahrus <learn.ahmadmahruz01.com>
2023-11-08 09:43:45 +0700

    add build push image

---
+++ build_push_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t ghcr.io/mahruz02/order-service:latest .
+
+# Log in to GitHub Container Registry
+echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u mahruz02 --password-stdin
+
+# Push Docker image to GitHub Container Registry
+docker push ghcr.io/mahruz02/order-service:latest
